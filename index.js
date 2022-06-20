a8f6d5bb4b65747572b0cb363bc4e5890a0ea68d
John Solomou <solomoufam@yahoo.com>
2022-06-20 09:25:37 -0400

    added static file using express

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
 var express = require("express");
+var multer = require("multer");
 var app = express();
-var things = require("./things.js");
+
 app.set("view engine", "pug");
 app.set("views", "./views");
-app.use("/things", things);
-app.get("/", function (req, res) {
-  res.send("Hello world!");
+app.use(express.static("public"));
+// for parsing application/json
+app.use(express.json());
+app.get("/", function (req, res, next) {
+  res.render("form");
 });
-app.post("/hello", function (req, res) {
-  res.send("You just called the post method at '/hello'!\n");
+// for parsing application/xwww-
+// app.use(express.urlencoded({ extended: true }));
+//form-urlencoded
+
+// for parsing multipart/form-data
+
+app.post("/", multer().none(), function (req, res) {
+  console.log(req.body);
+  res.send("received your request!");
 });
-app.get("/first_template", function (req, res) {
-  res.render("first_view");
-});
-// app.get("/:id", function (req, res) {
-//   res.send("The id you specified is " + req.params.id);
+app.listen(3000);
+
+// var express = require("express");
+// var app = express();
+// var things = require("./things.js");
+
+// app.set("view engine", "pug");
+// app.set("views", "./views");
+// app.use("/things", things);
+// app.use(express.static(__dirname + "/public"));
+// app.get("/", function (req, res) {
+//   res.send("Hello world!");
 // });
-app.get("/dynamic_view", function (req, res) {
-  res.render("dynamic", {
-    user: { name: "Johh", age: "40" },
-  });
-});
+// app.post("/hello", function (req, res) {
+//   res.send("You just called the post method at '/hello'!\n");
+// });
+// app.get("/first_template", function (req, res) {
+//   res.render("first_view");
+// });
+// // app.get("/:id", function (req, res) {
+// //   res.send("The id you specified is " + req.params.id);
+// // });
+// app.get("/static_file_test", function (req, res) {
+//   res.render("static_file_test");
+// });
+// app.get("/dynamic_view", function (req, res) {
+//   res.render("dynamic", {
+//     user: { name: "Johh", age: "40" },
+//   });
+// });
 // res.render("dynamic", {
 //   name: "TutorialsPoint",
 //   url: "http://www.tutorialspoint.com",
 // });
-app.get("/components", function (req, res) {
-  res.render("CONTENT");
-});
-//Simple request time logger
-app.use(function (req, res, next) {
-  console.log("A new request received at " + Date.now());
+// app.get("/components", function (req, res) {
+//   res.render("CONTENT");
+// });
+// //Simple request time logger
+// app.use(function (req, res, next) {
+//   console.log("A new request received at " + Date.now());
 
-  //This function call is very important. It tells that more processing is
-  //required for the current request and is in the next middleware function route handler.
-  next();
-});
-//Middleware function to log request protocol
-app.use("/things", function (req, res, next) {
-  console.log("A request for things received at " + Date.now());
-  next();
-});
-
-app.listen(3000);
+//   //This function call is very important. It tells that more processing is
+//   //required for the current request and is in the next middleware function route handler.
+//   next();
+// });
+// //Middleware function to log request protocol
+// app.use("/things", function (req, res, next) {
+//   console.log("A request for things received at " + Date.now());
+//   next();
+// });
